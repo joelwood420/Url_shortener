e7ecc0db1be5808f8376ab605d867e6c7eeafcf7
joelwood420 <joelww1556@gmail.com>
2026-02-05 13:06:07 +1300

    Add Dockerfile and Fly configuration; implement URL shortening API

---
+++ url_shortener/go.mod
@@ -0,0 +1,5 @@
+module url_shortener
+
+go 1.25.6
+
+require github.com/google/uuid v1.6.0
--- url_shortener/main.go
+++ url_shortener/main.go
@@ -3,40 +3,65 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"net/http"
+	"strings"
 )
 
 type ShorterRequest struct {
-	Url string
-	Tag string
+	Url string `json:"Url"`
+	Tag string `json:"Tag"`
 }
+
+type ShorterResponse struct {
+	ShortURL string `json:"short_url"`
+}
+
+var urlMap = make(map[string]string) // In-memory storage: short_code -> long_url
 
 func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello, world"))
+		w.Write([]byte("URL Shortener API"))
 	})
 
 	router.HandleFunc("POST /shorten", func(w http.ResponseWriter, r *http.Request) {
-
-		requestData := ShorterRequest{}
-		fmt.Println("Received request") //debug
-
+		var requestData ShorterRequest
 		err := json.NewDecoder(r.Body).Decode(&requestData)
 		if err != nil {
-			fmt.Println("JSON decode error:", err) //debug
-			panic(err)
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
 		}
 
-		w.Write([]byte(requestData.Url))
+		shortCode := generateShortURL(requestData.Url)
+		urlMap[shortCode] = requestData.Url
+
+		response := ShorterResponse{ShortURL: fmt.Sprintf("https://url-shortener-damp-pond-9136.fly.dev/%s", shortCode)}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	router.HandleFunc("GET /{shortCode}", func(w http.ResponseWriter, r *http.Request) {
+		shortCode := r.PathValue("shortCode")
+		longURL, exists := urlMap[shortCode]
+		if !exists {
+			http.Error(w, "Short URL not found", http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, longURL, http.StatusFound)
 	})
 
 	server := http.Server{
 		Addr:    ":3000",
 		Handler: router,
 	}
-	fmt.Println("Listening on port 3000")
+	fmt.Println("URL Shortener listening on port 3000")
 	server.ListenAndServe()
+}
 
+func generateShortURL(longURL string) string {
+	id := uuid.New()
+	shortCode := strings.ReplaceAll(id.String()[:8], "-", "")
+	return shortCode
 }
